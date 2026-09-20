eda01a1105cf7aa60f80e65a077d6c570eb4ce8c
misiektoja <misiektoja-github@rm-rf.ninja>
2026-09-21 01:46:46 +0200

    build: declare version 2.0

--- psn_monitor.py
+++ psn_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v1.9.1
+v2.0
 
 Tool implementing real-time tracking of Sony PlayStation (PSN) players activities:
 https://github.com/misiektoja/psn_monitor/
@@ -18,7 +18,7 @@ wcwidth (optional, measures wide characters correctly when TRUNCATE_CHARS is set
 colorama (optional, for better colours on Windows terminals)
 """
 
-VERSION = "1.9.1"
+VERSION = "2.0"
 
 # ---------------------------
 # CONFIGURATION SECTION START
